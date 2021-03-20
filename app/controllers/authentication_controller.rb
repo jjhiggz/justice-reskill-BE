5448cf953d84a6933d27f1370b3d376ef354fa6a
jjhiggz <jonathan.higger@gmail.com>
2021-03-20 10:27:37 -0600

    fixed auth error, but need to go back to implement auth

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,12 +1,12 @@
 class AuthenticationController < ApplicationController
-    def authenticate
-        @user = !User.find_by({email: user_params[:email]})
-        if !@user
+    # def authenticate
+    #     @user = !User.find_by({email: user_params[:email]})
+    #     if !@user
         
-        elseif
+    #     elseif
 
-        end
-    end
+    #     end
+    # end
 
     def user_params
         params.require(:user).permit(:email, :password)
